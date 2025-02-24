54453680d94eb785c290d065e0e7c14cbe1445c4
argonmining <ashton@onargon.com>
2025-02-23 23:47:52 -0800

    test releases

---
+++ rollup.config.mjs
@@ -0,0 +1,41 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import postcss from "rollup-plugin-postcss";
+import dts from "rollup-plugin-dts";
+
+const config = [
+  {
+    input: "src/components/index.ts",
+    output: [
+      {
+        file: "dist/index.js",
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+      postcss({
+        config: {
+          path: "./postcss.config.js",
+        },
+        extensions: [".css"],
+        minimize: true,
+        inject: {
+          insertAt: "top",
+        },
+      }),
+    ],
+    external: ["react", "react-dom", "@headlessui/react", "@heroicons/react"],
+  },
+  {
+    input: "dist/types/components/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+  },
+];
+
+export default config; 
